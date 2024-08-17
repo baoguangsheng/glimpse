91a45380556f54ba6075c1a7559f1ecc7f85007c
Bao Guangsheng <gshbao@hotmail.com>
2024-08-17 14:59:00 +0800

    customize rank_size for each estimator

--- main.sh
+++ main.sh
@@ -22,15 +22,17 @@ api_endpoint="https://xxxx.openai.azure.com/"
 api_key="xxxxxxxx"
 api_version="2024-02-15-preview"
 scoring_models="babbage-002:prompt3 davinci-002:prompt3 gpt-35-turbo-1106:prompt3 gpt-4-1106:prompt4"
+estimators="geometric:1000 zipfian:100 mlp:100"
 
-for estimator in geometric zipfian mlp; do
+for ER in $estimators; do
+  IFS=':' read -r -a ER <<< $ER && E=${ER[0]} && R=${ER[1]}
   for M in $source_models; do
     for D in $datasets; do
       for S in $scoring_models; do
         IFS=':' read -r -a S <<< $S && M1=${S[0]} && P=${S[1]}
-        echo `date`, "Evaluating PDE (${estimator}) on ${D}_${M}.${M1} with ${P} ..."
+        echo `date`, "Evaluating PDE (${E}:${R}) on ${D}_${M}.${M1} with ${P} ..."
         python scripts/probability_distribution_estimation.py --api_endpoint $api_endpoint --api_version $api_version \
-                                  --api_key $api_key --scoring_model_name $M1 --prompt $P --estimator $estimator \
+                                  --api_key $api_key --scoring_model_name $M1 --prompt $P --estimator $E --rank_size $R \
                                   --dataset $D --dataset_file $data_path/${D}_${M} --output_file $res_path/${D}_${M}.${M1}
       done
     done
